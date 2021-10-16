5d0833baa06c675549a4f2bf96fb625296f31adf
kipoka <ladykipoka@gmail.com>
2021-10-16 13:17:48 +0200

    first commit

---
+++ pizzadatainterface/PizzaDataInterface.java
@@ -0,0 +1,46 @@
+package za.ac.tut.pizzadatainterface;
+
+/**
+ *
+ * @author acer
+ */
+public interface PizzaDataInterface {
+    
+    public double PRICE_MINCED_MEAT_SMALL = 21.50;
+    public double PRICE_MINCED_MEAT_MEDIUM =  31.00;
+    public double PRICE_MINCED_MEAT_LARGE = 78.50;
+    
+    public double PRICE_PEPPERONI_SMALL = 49.50;
+    public double PRICE_PEPPERONI_MEDIUM = 79.50;
+    public double PRICE_PEPPERONI_LARGE = 80.50;
+    
+    public double PRICE_PICCOLO_HEART_SMALL = 51.50;
+    public double PRICE_PICCOLO_HEART_MEDIUM = 79.50;
+    public double PRICE_PICCOLO_HEART_LARGE = 80.50;
+    
+    public double PRICE_VEGGIES_SMALL = 39.50;
+    public double PRICE_VEGGIES_MEDIUM = 69.50;
+    public double PRICE_VEGGIES_LARGE = 85.50;
+      
+    public double FRUIT_SALAD_PRICE = 30.50;
+    public double CHINESE_CHICKEN_SALAD_PRICE = 37.00;
+    public double CHEESE_SLAW_PRICE= 76.00;
+    
+    public double PRICE_ORANGE_JUICE_SMALL= 19.00;
+    public double PRICE_ORANGE_JUICE_MEDIUM = 29.00;
+     public double PRICE_ORANGE_JUICE_LARGE = 50.00;
+     
+    public double PRICE_COKE_SMALL= 11.00;
+    public double PRICE_COKE_MEDIUM= 20.00;
+    public double PRICE_COKE_LARGE= 32.00;
+    
+    public double PRICE_WATERMELON_SMALL= 18.00;
+    public double PRICE_WATERMELON_MEDIUM= 25.00;
+    public double PRICE_WATERMELON_LARGE= 41.00;
+   
+    public double PRICE_MIXED_FRUIT_SMALL= 15.00;
+    public double PRICE_MIXED_FRUIT_MEDIUM= 19.00;
+    public double PRICE_MIXED_FRUIT_LARGE= 39.00;
+    
+    
+}
---
+++ pizzamanager/ProcessManager.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package za.ac.tut.pizzamanager;
+
+import javax.swing.JFrame;
+
+/**
+ *
+ * @author acer
+ */
+public class ProcessManager extends JFrame{
+       
+    
+    public void determineAMountDue(){
+        String size, name;
+        int price;
+        
+    }
+    
+    
+    
+}
